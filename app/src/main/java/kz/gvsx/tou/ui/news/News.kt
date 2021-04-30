5d16045d8279e633cb72c014bef32587dda2cd7d
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2021-04-30 20:57:22 +0600

    Initial commit

---
+++ app/src/main/java/kz/gvsx/tou/ui/news/News.kt
@@ -0,0 +1,11 @@
+package kz.gvsx.tou.ui.news
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class News(
+    val imageUrl: String,
+    val title: String,
+    val dateTime: LocalDateTime,
+    val linkToArticle: String
+)
